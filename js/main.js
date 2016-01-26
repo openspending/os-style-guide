1d7fe455a11e485b9654703106e3bac58ac80b3a
Sam Smith <sam@mintcanary.com>
2016-01-26 10:02:56 +0000

    modal WIP

--- js/main.js
+++ js/main.js
@@ -35,4 +35,27 @@ jQuery(document).ready(function($){
     jQuery(this).parent().addClass('is-expanded');
   });
 
+
+  /*******************
+    modal
+  ********************/
+
+  $(function() {
+    $(".modal-state").on("change", function() {
+      if ($(this).is(":checked")) {
+        $("body").addClass("modal-open");
+      } else {
+        $("body").removeClass("modal-open");
+      }
+    });
+
+    $(".modal-fade-screen, .modal-close").on("click", function() {
+      $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal").on("click", function(e) {
+      e.stopPropagation();
+    });
+  });
+
 });
